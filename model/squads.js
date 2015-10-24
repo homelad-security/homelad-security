652a40834845105a82407d2adc9ec193b20a2f2b
David Wood <david.p.wood@gmail.com>
2015-10-24 19:46:51 +0100

    Add Facebook login

--- model/squads.js
+++ model/squads.js
@@ -1,1 +1,13 @@
 Squads = new Mongo.Collection("squads");
+
+Squads.allow({
+  insert: function (userId, squad) {
+    return userId && squad.owner === userId;
+  },
+  update: function (userId, squad, fields, modifier) {
+    return userId && squad.owner === userId;
+  },
+  remove: function (userId, squad) {
+    return userId && squad.owner === userId;
+  }
+});
