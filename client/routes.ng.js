9a1303525003954557f373d237a814dba856b9d2
David Wood <david.p.wood@gmail.com>
2015-10-24 21:34:17 +0100

    Add semantic ui broken

--- client/routes.ng.js
+++ client/routes.ng.js
@@ -7,6 +7,11 @@ angular.module("homeladsecurity").config(function ($urlRouterProvider, $statePro
       templateUrl: 'client/squads/views/squads-list.ng.html',
       controller: 'SquadsListCtrl'
     })
+    .state('squads', {
+      url: '/squads',
+      templateUrl: 'client/squads/views/squads-list.ng.html',
+      controller: 'SquadsListCtrl'
+    })
     .state('squadDetails', {
       url: '/squads/:squadId',
       templateUrl: 'client/squads/views/squad-details.ng.html',
