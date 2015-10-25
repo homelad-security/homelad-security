e20ecb028bfcafcdbabd107773d03ee09910e19b
Steven Stinson <steven.stinson@esendex.com>
2015-10-25 11:37:08 +0000

    Add my squads to home page

--- client/home/controllers/home.ng.js
+++ client/home/controllers/home.ng.js
@@ -1,3 +1,9 @@
 angular.module('homeladsecurity').controller('HomeCtrl', function ($scope, $stateParams, $meteor) {
   $scope.isHome = true;
+  $scope.userId = function(){
+    return $meteor.userId();
+  };
+  $scope.squads = $meteor.collection(Squads, {
+        'members.userid': $scope.userId
+  });
 });
