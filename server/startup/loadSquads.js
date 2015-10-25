e967dfbb0241ff6c92ebc587370283d426776a43
David Wood <david.p.wood@gmail.com>
2015-10-25 11:43:25 +0000

    Merging this

--- server/startup/loadSquads.js
+++ server/startup/loadSquads.js
@@ -1,11 +1,11 @@
 Meteor.startup(function () {
   if (Squads.find().count() === 0) {
     var squads = [
-      {'name': 'Alpha'},
-      {'name': 'Beta'},
-      {'name': 'Gamma'}
+      {'name': 'Alpha', 'description': 'East End LADs'},
+      {'name': 'Beta', 'description': 'The safety LADS'},
+      {'name': 'Gamma', 'description': 'LADp'}
     ];
     for (var i = 0; i < squads.length; i++)
-      Squads.insert({name: squads[i].name});
+      Squads.insert(squads[i]);
   }
 });
